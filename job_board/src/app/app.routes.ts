288f9b5fc448834d1d2f7c5af49dddd026ec7d4c
ballgimn <meytopintheworld@gmail.com>
2025-04-02 01:17:20 +0500

    laila vacancies first push

---
+++ job_board/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { VacanciesComponent } from './pages/vacancies/vacancies.component';
+
+export const routes: Routes = [
+  { path: 'vacancies', component: VacanciesComponent },
+  { path: '', redirectTo: '/vacancies', pathMatch: 'full' }  
+];
